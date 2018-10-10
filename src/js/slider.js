586ee2ebfc8453b30dc7fb3c94eb008d5dd92f4d
Aleksandr <11nav09alex1mo@gmail.com>
2018-10-10 22:40:03 +0300

    eddit slider 22:39

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,40 +4,26 @@
   const BLOCK = document.querySelector("#slider .active").firstElementChild;
   var max_width = BLOCK.childElementCount * parseFloat(getComputedStyle(BLOCK).width);
   var width = parseFloat(getComputedStyle(BLOCK).width)
+  var num = 0;
 
 
   var pos = 0;
   RIGHT.addEventListener("click", function (e) {
-    max_width_new = BLOCK.childElementCount * parseFloat(getComputedStyle(BLOCK).width);
-    width_new = parseFloat(getComputedStyle(BLOCK).width)
-    if (width_new < width || width_new > width) {
-      if (pos === 0) {
-        pos += width_new - width
-      }
-      pos = pos - (width_new - width);
-      width = width_new;
-      max_width = max_width_new
-    }
     pos = pos - width <= -max_width ? 0 : pos - width;
     BLOCK.style.left = pos + "px";
-    console.log(pos)
+    num = num + 1 > BLOCK.childElementCount - 1 ? 0 : num + 1;
   })
 
   LEFT.addEventListener("click", function (e) {
-    max_width_new = BLOCK.childElementCount * parseFloat(getComputedStyle(BLOCK).width);
-    width_new = parseFloat(getComputedStyle(BLOCK).width)
-    if (width_new < width || width_new > width) {
-      if (pos === -(max_width - width)) {
-        pos -= width_new - width
-      }
-      pos = pos - (width_new - width);
-      width = width_new;
-      max_width = max_width_new
-    }
     pos = pos + width > 0 ? -max_width + width : pos + width;
     BLOCK.style.left = pos + "px";
-    console.log(pos)
-
+    num = num - 1 < 0 ? BLOCK.childElementCount - 1 : num - 1;
   })
 
+  window.onresize = function (e) {
+    max_width = BLOCK.childElementCount * parseFloat(getComputedStyle(BLOCK).width);
+    width = parseFloat(getComputedStyle(BLOCK).width);
+    pos = -(width * num);
+    BLOCK.style.left = pos + "px";
+  };
 })()
